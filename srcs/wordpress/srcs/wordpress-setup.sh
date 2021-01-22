c19eef0bec01fd14ce573faa13240135ce7c8497
Daniel Rodriguez pueyo <danrodri@c1r9s6.42madrid.com>
2021-01-22 21:40:19 +0100

    minor changes

--- srcs/wordpress/srcs/wordpress-setup.sh
+++ srcs/wordpress/srcs/wordpress-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 rc-service telegraf start
 rc-service php-fpm7 start
+cd /tmp
+mv wordpress/* /var/www/html/
+rm -rf wordpress
 cd /var/www/
 sed -i -e "s/db_name/$(echo $DB_NAME)/" /var/www/wp-config.php
 sed -i -e "s/db_user/$(echo $DB_USER)/" /var/www/wp-config.php 
