44837a0c816704cca96e1bc0ac9c8f72a3310493
Daniel Rodriguez pueyo <danrodri@c1r10s1.42madrid.com>
2021-01-30 20:47:05 +0100

    changed wp server from nginx to lighttpd

--- srcs/wordpress/srcs/wordpress-setup.sh
+++ srcs/wordpress/srcs/wordpress-setup.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 rc-service telegraf start
-rc-service php-fpm7 start
-cd /tmp
-mv wordpress/* /var/www/localhost/htdocs/
-rm -rf wordpress
-cd /var/www/
+mv -v /tmp/wordpress/* /var/www/localhost/htdocs/
+rm -rf /tmp/wordpress
 sed -i -e "s/db_name/$(echo $DB_NAME)/" /var/www/localhost/htdocs/wp-config.php
 sed -i -e "s/db_user/$(echo $DB_USER)/" /var/www/localhost/htdocs/wp-config.php 
-sed -i -e "s/db_password/$(echo $DB_PASSWORD)/" /var/www/localhost/htdocs/wp-config.php 
-nginx -g 'daemon off;'
+sed -i -e "s/db_password/$(echo $DB_PASSWORD)/" /var/www/localhost/htdocs/wp-config.php
+rc-service lighttpd start
+tail -f /var/log/lighttpd/error.log
