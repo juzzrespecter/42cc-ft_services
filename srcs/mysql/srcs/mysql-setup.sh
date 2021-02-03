ae6999c37b40c6ad8afcbd4de880cfe777bd745a
Dani <danisarp@gmail.com>
2021-02-03 16:37:03 +0100

    up to date

--- srcs/mysql/srcs/mysql-setup.sh
+++ srcs/mysql/srcs/mysql-setup.sh
@@ -6,9 +6,9 @@ mysql -u root < /tmp/mysql-config.sql
 echo "CREATE DATABASE IF NOT EXISTS wp_database;" | mysql -u root;
 echo "GRANT ALL PRIVILEGES ON wp_database.* TO '$(echo $WP_ADMIN)'@'%' IDENTIFIED BY '$(echo $WP_PASSWORD)';" | mysql -u root;
 echo "GRANT ALL PRIVILEGES ON *.* TO '$(echo $PMA_USER)'@'%' IDENTIFIED BY '$(echo $PMA_PASSWD)';" | mysql -u root;
+mysql -u root < /tmp/create_tables.sql
+mysql -u root < /tmp/wp_database.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(echo $MYSQL_ROOT_PASSWD)';" | mysql -u root;
 echo "DROP DATABASE test;" | mysql -u root;
 echo "FLUSH PRIVILEGES;" | mysql -u root;
-mysql -u root -p $MYSQL_ROOT_PASSWD < /tmp/create_tables.sql
-mysql -u root -p $MYSQL_ROOT_PASSWD < /tmp/wp_database.sql
 mysqld_safe --datadir=/var/lib/mysql
