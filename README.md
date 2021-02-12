5bf545e325d4a806607a9bceac3c1d3074981858
Dani <danisarp@gmail.com>
2021-02-12 15:46:10 +0100

    grafana & ports

---
+++ README.md
@@ -0,0 +1,2 @@
+# ft_services
+Part of 42 common-core cursus: 
--- srcs/grafana/srcs/grafana-setup.sh
+++ srcs/grafana/srcs/grafana-setup.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -newkey rsa:4096 \
-	-keyout /usr/local/share/ca-certificates/ssl-key.key \
-   	-out /usr/local/share/ca-certificates/ssl-cert.crt -days 365 \
-	-subj "/CN=influxdb-svc" \
-	-addext "subjectAltName = DNS:influxdb-svc";
-
 update-ca-certificates
-
-cd /grafana/bin/
+nginx
 telegraf --config=/etc/telegraf/telegraf.conf &
 ./grafana-server
